358c156fd4cef4d5148e8fc12e20a31e5b228409
Azaria Gebremichael <azariagebremichael10@gmail.com>
2020-11-30 14:25:56 +0300

    file upload jquery add and index.html upload button

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        print('f:  ',f)
+        return None
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0',debug=True, port=port)
 
