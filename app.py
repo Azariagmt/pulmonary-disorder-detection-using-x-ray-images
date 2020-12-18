ba01d9e11c42d1c9a22641f869622f6eb8d9a432
Azaria Gebremichael <azariagebremichael10@gmail.com>
2020-12-19 02:49:14 +0300

    app modify

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ def model_from_drive():
     global MODEL_PATH
     # https://drive.google.com/file/d/1-2cRvRkTnWNoGv6pNTAVBiC72-4BoVO0/view?usp=sharing
     # https://drive.google.com/file/d/18691v06KVU_TsFfN5WceJZ6sDS8tjYaG/view?usp=sharing
+    # https://drive.google.com/file/d/16Tg8vU7IpB7Xg8hvrAEAsyz8OrjlEQvv/view?usp=sharing
     from google_drive_downloader import GoogleDriveDownloader as gdd
-    gdd.download_file_from_google_drive(file_id='18691v06KVU_TsFfN5WceJZ6sDS8tjYaG',
+    gdd.download_file_from_google_drive(file_id='16Tg8vU7IpB7Xg8hvrAEAsyz8OrjlEQvv',
         dest_path='./model/binary-covid-model-6.h5',
     )
     MODEL_PATH = 'model/binary-covid-model-6.h5'
@@ -22,7 +23,7 @@ def model_from_local():
     global MODEL_PATH
     MODEL_PATH = 'models/covidet.h5'
     
-model_from_local()
+model_from_drive()
 # Load your trained model
 model = load_model(MODEL_PATH)
 
